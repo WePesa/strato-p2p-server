bd9e7ab80aff930c5e9b919eeca7e3f79acc581d
Jamshid <qq@qq.com>
2016-01-17 09:39:09 -0800

    fixing trigger bugs....  Got rid of feedback look for blocks, and filtered out partial blocks from being passed on to peers

--- src/Blockchain/BlockNotify.hs
+++ src/Blockchain/BlockNotify.hs
@@ -17,17 +17,18 @@ import           Control.Monad
 import Blockchain.Data.BlockDB
 import Blockchain.Data.DataDefs
 import Blockchain.DB.SQLDB
+import Blockchain.SHA
 
 createBlockTrigger :: PS.Connection -> IO ()
 createBlockTrigger conn = do
-     res2 <- PS.execute_ conn "DROP TRIGGER IF EXISTS p2p_block_notify ON block;\n\
+     res2 <- PS.execute_ conn "DROP TRIGGER IF EXISTS p2p_block_notify ON block_data_ref;\n\
 \CREATE OR REPLACE FUNCTION p2p_block_notify() RETURNS TRIGGER AS $p2p_block_notify$ \n\ 
     \ BEGIN \n\
     \     PERFORM pg_notify('p2p_new_block', NEW.id::text ); \n\
     \     RETURN NULL; \n\
     \ END; \n\
 \ $p2p_block_notify$ LANGUAGE plpgsql; \n\
-\ CREATE TRIGGER p2p_block_notify AFTER INSERT OR DELETE ON block FOR EACH ROW EXECUTE PROCEDURE p2p_block_notify();"
+\ CREATE TRIGGER p2p_block_notify AFTER INSERT OR UPDATE ON block_data_ref FOR EACH ROW EXECUTE PROCEDURE p2p_block_notify();"
 
      putStrLn $ "created trigger with result: " ++ (show res2)
 
@@ -39,21 +40,22 @@ blockNotificationSource pool conn = forever $ do
     liftIO $ putStrLn $ "about to listen for new block notifications"
     rowId <- liftIO $ fmap (SQL.toSqlKey . read . BC.unpack . notificationData) $ getNotification conn
     liftIO $ putStrLn $ "######## new block has arrived: rowid=" ++ show rowId
-    maybeTx <- lift $ getBlockFromKey pool rowId
-    case maybeTx of
+    maybeBlock <- lift $ getBlockFromKey pool rowId
+    case maybeBlock of
      Nothing -> error "wow, item was removed in notificationSource before I could get it....  This didn't seem like a likely occurence when I was programming, you should probably deal with this possibility now"
-     Just (tx, difficulty) -> yield (tx, difficulty)
+     Just (b, difficulty, hash) -> do
+       when (hash /= SHA 1) $ yield (b, difficulty)
 
-getBlockFromKey::SQLDB->SQL.Key Block->IO (Maybe (Block, Integer))
+getBlockFromKey::SQLDB->SQL.Key BlockDataRef->IO (Maybe (Block, Integer, SHA))
 getBlockFromKey pool row = do
     --pool <- getSQLDB      
-    maybeBlock <- SQL.runSqlPool (SQL.get row) pool
-    case maybeBlock of
+    maybeBlockDataRef <- SQL.runSqlPool (SQL.get row) pool
+    case maybeBlockDataRef of
      Nothing -> return Nothing
-     Just b -> do
-       bdList <-
+     Just bd -> do
+       maybeBlock <-
          flip SQL.runSqlPool pool $ do
-           SQL.selectList [BlockDataRefHash SQL.==. blockHash b] []
-       case bdList of
-        [bd] -> return $ Just (b, blockDataRefTotalDifficulty $ SQL.entityVal bd)
-        _ -> error "block missing blockData in call to getBlockFromKey"
+           SQL.get (blockDataRefBlockId bd)
+       case maybeBlock of
+        Just b -> return $ Just (b, blockDataRefTotalDifficulty bd, blockDataRefHash bd)
+        Nothing -> error "block missing blockData in call to getBlockFromKey"
