91432e6cd5972a5873ec494e8578a52b10b020e2
Jamshid <qq@qq.com>
2016-05-11 11:53:47 -0700

    Adding ethereum-index to strato, and cleaning up options.

--- exec_src/Strato.hs
+++ exec_src/Strato.hs
@@ -4,12 +4,18 @@ import Control.Monad.Logger
 import Control.Concurrent
 import HFlags
 
+import Blockchain.IOptions
+import Blockchain.Mining.Options
 import Blockchain.Output
 import Blockchain.Options ()
---import Blockchain.Quarry.Flags ()
+import Blockchain.Quarry.Flags ()
 import Blockchain.ServOptions
+import Blockchain.VMOptions ()
+
 import Executable.EthereumDiscovery
+import Executable.EthereumVM
 import Executable.StratoAdit
+import Executable.StratoIndex
 import Executable.StratoP2PClient
 import Executable.StratoP2PServer
 import Executable.StratoQuary
@@ -30,5 +36,7 @@ main = do
 
   forkIO $ flip runLoggingT printLogMsg $ stratoQuary
   forkIO $ flip runLoggingT printLogMsg $ stratoAdit
+  forkIO $ flip runLoggingT printLogMsg $ ethereumVM
+  forkIO $ flip runLoggingT printLogMsg $ stratoIndex
   forkIO $ flip runLoggingT printLogMsg $ stratoP2PClient args
   flip runLoggingT printLogMsg stratoP2PServer
