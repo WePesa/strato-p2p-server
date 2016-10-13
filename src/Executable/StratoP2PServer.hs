96537a59e85c4a7b5e80d4c936d683fe72c7cec5
Ryan Reich <ryan.reich@gmail.com>
2016-10-12 22:11:46 -0700

    Introduce global private key so that the client and server have the same identity

--- src/Executable/StratoP2PServer.hs
+++ src/Executable/StratoP2PServer.hs
@@ -12,17 +12,13 @@ import Blockchain.EthConf
 import Blockchain.ServOptions
 import Blockchain.TCPServer
 
-privateKey :: Integer
-privateKey =  0xac3e8ce2ef31c3f45d5da860bcd9aee4b37a05c5a3ddee40dd061620c3dab380
-
 stratoP2PServer:: LoggingT IO ()
 stratoP2PServer = do
   logInfoN $ T.pack $ "connect address: " ++ (flags_address)
   logInfoN $ T.pack $ "listen port:     " ++ (show flags_listen)
 
-  let myPriv = privateKey
---      myPublic = calculatePublic theCurve (fromIntegral myPriv)
-  
+  let PrivKey myPriv = privKey ethConf
+
   _ <- runResourceT $ do
           runEthServer connStr' myPriv flags_listen
   return ()
